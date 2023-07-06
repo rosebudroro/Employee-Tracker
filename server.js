c70b482eb44443b17c0e97ae382567a497dd7bbb
Roxy Osorio <roxy.osorio@moo.com>
2023-07-05 20:16:08 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'movies_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
