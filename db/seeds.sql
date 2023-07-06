62950b95d256151b3a9d7ec89389e2449980dd3e
Roxy Osorio <roxy.osorio@moo.com>
2023-07-05 23:21:40 -0400

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (name)
+VALUES 
+('Engineering'),
+('Finance'),
+('Marketing'),
+('IT');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Front End Developer', 75000, 1),
+('Software Engineer', 120000, 1),
+('Accountant', 10000, 2), 
+('Stock Analyst', 150000, 2),
+('Marketing Manager', 120000, 3), 
+('Sales Lead', 90000, 3),
+('Cybersecurity Manager', 100000, 4),
+('UX Coordinator', 70000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Delwyn', 'Gamboa', 2, 3),
+('Sunny', 'Kingston', 1, null),
+('Maya', 'Alvarez', 4, 2),
+('Ivette', 'Chow', 3, null),
+('Joe', 'Sas', 6, 4),
+('Selena', 'Tolentino', 5, 5),
+('Diana', 'Walker', 7, 1),
+('Channa', 'Morris', 8, null);
